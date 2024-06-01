9486b291a67d14b4ef3ef70f20ea8eadc398b78c
Coder Talha <talhacust7@gmail.com>
2024-06-02 00:47:04 +0500

    navbar

--- src/Home/Pages/Page2.jsx
+++ src/Home/Pages/Page2.jsx
@@ -6,7 +6,7 @@ const Page2 = ({ sec2 }) => {
     <Section>
       <div
         ref={sec2}
-        className="section2 fontHorizon z-[2] lg:text-7xl text-2xl flex flex-col items-center justify-center pt-20 text-white leading-tight font-bold uppercase opacity-0 "
+        className="section2 fontHorizon z-[2] lg:text-7xl text-2xl flex flex-col items-center justify-center pt-40 lg:pt-20 text-white leading-tight font-bold uppercase opacity-0 "
       >
         <p className="">delve into the</p>
         <p className="text-3xl lg:text-9xl headline-orange">FUTURE</p>
